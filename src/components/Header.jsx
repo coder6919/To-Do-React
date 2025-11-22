e625249257f5cf1739a3e68307a0b23f8a350f88
Saifshr <ssaifssyed17@gmail.com>
2025-11-22 14:59:28 +0530

    added header component that displayes heading of the application

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+// src/components/Header.jsx
+import React from 'react';
+
+function Header() {
+  return (
+    <h1 className="text-3xl sm:text-4xl font-extrabold text-gray-800 text-center mb-6 sm:mb-10">
+      📝 My To-Do List
+    </h1>
+  );
+}
+
+export default Header;
